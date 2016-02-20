b1e89b08d4701515fee9a51fec69b453c1b5e1fa
Ivo Petkov <ivo@ivopetkov.com>
2016-02-20 16:18:11 +0200

    Added addon registration autoloader.

---
+++ autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * HTML Server Components addon for Bear Framework
+ * https://github.com/ivopetkov/html-server-components-bearframework-addon
+ * Copyright (c) 2016 Ivo Petkov
+ * Free to use under the MIT license.
+ */
+
+BearFramework\Addons::register('ivopetkov/html-server-components-bearframework-addon', __DIR__);
