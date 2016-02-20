64931564e6cf66cb6d85db53aea110c0637ccdda
Ivo Petkov <ivo@ivopetkov.com>
2016-02-20 16:17:36 +0200

    Comments update.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * HTML Server Components Bear Framework addon
+ * HTML Server Components addon for Bear Framework
  * https://github.com/ivopetkov/html-server-components-bearframework-addon
  * Copyright (c) 2016 Ivo Petkov
  * Free to use under the MIT license.
