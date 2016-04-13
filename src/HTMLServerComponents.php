c9bf0a86cccc8187589167e8c5abf7e251d33e46
Ivo Petkov <ivo@ivopetkov.com>
2016-04-13 13:32:00 +0300

    Removed InsertHTML method.

--- src/HTMLServerComponents.php
+++ src/HTMLServerComponents.php
@@ -61,36 +61,4 @@ class HTMLServerComponents
         }
     }
 
-    /**
-     * Inserts HTML code (or HTML Component) in the position specified
-     * @param string $target The HTML code where the insertion will be made
-     * @param string $html The HTML code that will be inserted
-     * @param string $position The position of the insertion
-     * @throws \InvalidArgumentException
-     * @return string The resulting HTML code (processed)
-     */
-    public function insertHTML($target, $html, $position = 'beforeBodyEnd')
-    {
-        if (!is_string($target)) {
-            throw new \InvalidArgumentException('');
-        }
-        if (!is_string($html)) {
-            throw new \InvalidArgumentException('');
-        }
-        if (!is_string($position)) {
-            throw new \InvalidArgumentException('');
-        }
-        //todo better
-        $target = $this->process($target);
-        $component = '<component src="data:base64,' . base64_encode($html) . '" />';
-        if ($position === 'afterBodyBegin') {
-            $result = str_replace('<body>', '<body>' . $component, $target);
-        } elseif ($position === 'beforeBodyEnd') {
-            $result = str_replace('</body>', $component . '</body>', $target);
-        } else {
-            throw new \InvalidArgumentException('');
-        }
-        return $this->process($result);
-    }
-
 }
