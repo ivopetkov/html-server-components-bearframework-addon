a2bfeaa99450e62c282084fc34f35d313d5bfa4f
Ivo Petkov <ivo@ivopetkov.com>
2019-01-13 05:05:36 +0200

    Added support for custom component tag names.

--- classes/HTMLServerComponents.php
+++ classes/HTMLServerComponents.php
@@ -16,11 +16,18 @@ class HTMLServerComponents
 {
 
     /**
-     * Stores the added aliases
+     * Stores the added aliases.
      * 
      * @var array 
      */
     private $aliases = [];
+
+    /**
+     * Stores the defined tags.
+     * 
+     * @var array 
+     */
+    private $tagNames = [];
 
     /**
      *
@@ -39,9 +46,22 @@ class HTMLServerComponents
      * @param string $original The original source name
      * @return IvoPetkov\BearFramework\Addons\HTMLServerComponents Instance of itself.
      */
-    public function addAlias(string $alias, string $original)
+    public function addAlias(string $alias, string $original): self
     {
-        $this->aliases[] = ['alias' => $alias, 'original' => $original];
+        $this->aliases[$alias] = $original;
+        return $this;
+    }
+
+    /**
+     * Defines a new tag
+     * 
+     * @param string $tagName The alias
+     * @param string $src The original source name
+     * @return IvoPetkov\BearFramework\Addons\HTMLServerComponents Instance of itself.
+     */
+    public function addTag(string $tagName, string $src): self
+    {
+        $this->tagNames[$tagName] = $src;
         return $this;
     }
 
@@ -54,19 +74,31 @@ class HTMLServerComponents
      */
     public function process($content, array $options = [])
     {
-        if ((is_string($content) && strpos($content, '<component') !== false) || $content instanceof \IvoPetkov\HTMLServerComponent) {
+        $found = false;
+        if (is_string($content)) {
+            $tagNames = array_keys($this->tagNames);
+            $tagNames[] = 'component';
+            foreach ($tagNames as $tagName) {
+                if (strpos($content, '<' . $tagName) !== false) {
+                    $found = true;
+                    break;
+                }
+            }
+        }
+        if ($content instanceof \IvoPetkov\HTMLServerComponent) {
+            $found = true;
+        }
+        if ($found) {
             if (self::$newCompilerCache === null) {
                 self::$newCompilerCache = new \IvoPetkov\BearFramework\Addons\HTMLServerComponents\Internal\Compiler();
             }
             $compiler = clone(self::$newCompilerCache);
-            foreach ($this->aliases as $alias) {
-                $compiler->addAlias($alias['alias'], $alias['original']);
+            foreach ($this->aliases as $alias => $original) {
+                $compiler->addAlias($alias, $original);
             }
-//            $app = \BearFramework\App::get();
-//            if (!isset($options['variables'])) {
-//                $options['variables'] = [];
-//            }
-//            $options['variables']['app'] = $app;
+            foreach ($this->tagNames as $tagName => $src) {
+                $compiler->addTag($tagName, $src);
+            }
             return $compiler->process($content, $options);
         }
         return $content;
--- tests/ComponentsTest.php
+++ tests/ComponentsTest.php
@@ -30,7 +30,7 @@ class ComponentsTest extends BearFramework\AddonTests\PHPUnitTestCase
     {
         $app = $this->getApp();
 
-        $content = '<!DOCTYPE html><html><head></head><body>content</body></html>';
+        $content = '<!DOCTYPE html>' . "\n" . '<html><head></head><body>content</body></html>';
         $result = $app->components->process('<component src="data:base64,' . base64_encode($content) . '"></component>');
         $this->assertTrue($result === $content);
     }
@@ -42,9 +42,22 @@ class ComponentsTest extends BearFramework\AddonTests\PHPUnitTestCase
     {
         $app = $this->getApp();
 
-        $content = '<!DOCTYPE html><html><head></head><body>content</body></html>';
+        $content = '<!DOCTYPE html>' . "\n" . '<html><head></head><body>content</body></html>';
         $app->components->addAlias('newContent', 'data:base64,' . base64_encode($content));
         $result = $app->components->process('<component src="newContent" />');
+        $this->assertTrue($result === $content);
+    }
+
+    /**
+     * 
+     */
+    public function testTags()
+    {
+        $app = $this->getApp();
+
+        $content = '<!DOCTYPE html>' . "\n" . '<html><head></head><body>content</body></html>';
+        $app->components->addTag('new-content', 'data:base64,' . base64_encode($content));
+        $result = $app->components->process('<new-content/>');
         $this->assertTrue($result === $content);
     }
 
@@ -60,7 +73,7 @@ class ComponentsTest extends BearFramework\AddonTests\PHPUnitTestCase
                 . '$context = $app->context->get(__FILE__); '
                 . '?><!DOCTYPE html><html><head></head><body><?= get_class($context);?><?= realpath($context->dir);?><?= $component->innerHTML;?></body></html>');
         $result = $app->components->process('<component src="file:' . $app->config->appDir . '/component1.php">text1</component>');
-        $expectedResult = '<!DOCTYPE html><html><head></head><body>BearFramework\App\Context' . realpath($app->config->appDir) . 'text1</body></html>';
+        $expectedResult = '<!DOCTYPE html>' . "\n" . '<html><head></head><body>BearFramework\App\Context' . realpath($app->config->appDir) . 'text1</body></html>';
         $this->assertTrue($result === $expectedResult);
 
         $this->makeFile($app->config->addonsDir . '/vendor1/addon1/component1.php', '<?php '
@@ -71,7 +84,7 @@ class ComponentsTest extends BearFramework\AddonTests\PHPUnitTestCase
         \BearFramework\Addons::register('vendor1/addon1', $app->config->addonsDir . '/vendor1/addon1/');
         $app->addons->add('vendor1/addon1');
         $result = $app->components->process('<component src="file:' . $app->config->addonsDir . '/vendor1/addon1/component1.php">text1</component>');
-        $expectedResult = '<!DOCTYPE html><html><head></head><body>BearFramework\App\Context' . realpath($app->config->addonsDir . '/vendor1/addon1') . 'text1</body></html>';
+        $expectedResult = '<!DOCTYPE html>' . "\n" . '<html><head></head><body>BearFramework\App\Context' . realpath($app->config->addonsDir . '/vendor1/addon1') . 'text1</body></html>';
         $this->assertTrue($result === $expectedResult);
     }
 
@@ -87,7 +100,7 @@ class ComponentsTest extends BearFramework\AddonTests\PHPUnitTestCase
 
         $app->components->addAlias('component1', 'file:' . $app->config->appDir . '/component1.php');
 
-        $expectedResult = '<!DOCTYPE html><html><head></head><body>content1content2</body></html>';
+        $expectedResult = '<!DOCTYPE html>' . "\n" . '<html><head></head><body>content1content2</body></html>';
 
         $result = $app->components->process('<component src="file:' . $app->config->appDir . '/component1.php" />');
         $this->assertTrue($result === $expectedResult);
